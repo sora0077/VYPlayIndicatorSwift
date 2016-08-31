104e1e1fdcba6e494d2ec3aebbe66df158320e9e
林達也 <t.hayashi0077+github@gmail.com>
2016-08-31 23:00:35 +0900

    Initial Commit

---
+++ VYPlayIndicatorSwift/VYPlayIndicatorSwift.h
@@ -0,0 +1,19 @@
+//
+//  VYPlayIndicatorSwift.h
+//  VYPlayIndicatorSwift
+//
+//  Created by 林達也 on 2016/08/31.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VYPlayIndicatorSwift.
+FOUNDATION_EXPORT double VYPlayIndicatorSwiftVersionNumber;
+
+//! Project version string for VYPlayIndicatorSwift.
+FOUNDATION_EXPORT const unsigned char VYPlayIndicatorSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VYPlayIndicatorSwift/PublicHeader.h>
+
+
